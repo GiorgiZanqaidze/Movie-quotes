b47f01788b6a2a8426ae50330dfc62af183e90fa
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-04-03 15:49:28 +0400

    style: test movie page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\QuoteController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [QuoteController::class, 'index']);
-Route::get('/quotes/quote', [MovieController::class, 'quote']);
+Route::get('/quotes/quote', [MovieController::class, 'show']);
