3a05c99888c5342d9766cd6137f593176b8d6baf
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-04-05 17:27:42 +0400

    style: quote pages

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -1,29 +1,20 @@
 <x-layout>
-    <div class="">
+    <div class="flex flex-col">
         <p class="mb-4 text-center block text-3xl text-white pt-4">{{$movie->title}}</p>
         <div class="grid gap-4 grid-cols-1">
-        @if ($movie->quotes->count())
-            @foreach ($movie->quotes as $quote)
-            <div class="flex  items-center justify-center flex-col mt-10 mb-10">
-                <div class="block max-w-sm rounded-lg text-white dark:bg-neutral-700 text-black bg-white" >
-                    <img class="rounded-t-lg shadow-lg w-full"
-                        src="{{asset('storage/' . $movie->thumbnail)}}"
-                        alt="" />
-                    <div class="p-6">
-                </div>
-                <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200 text-3xl text-black">{{$quote->name}}</p>
-            </div>
-            @endforeach
-        @else
-       <div class="flex items-center justify-center flex-col mt-10 mb-10">
-                <div class="block max-w-sm rounded-lg text-white dark:bg-neutral-700 text-black bg-white" >
-                    <img class="rounded-t-lg shadow-lg w-full"
-                        src="{{asset('storage/' . $movie->thumbnail)}}"
-                        alt="" />
-                    <div class="p-6">
-                </div>
-            </div>
-        @endif 
+            @if ($movie->quotes->count())
+                @foreach ($movie->quotes as $quote)
+                    <div class="flex  items-center justify-center flex-col mt-10 mb-10">
+                        <div class="block max-w-sm rounded-lg text-white dark:bg-neutral-700 text-black bg-white w-full" >
+                            <img class="rounded-t-lg shadow-lg w-full"
+                                src="{{asset('storage/' . $quote->thumbnail)}}"
+                                alt="" />
+                            <div class="p-6">
+                        </div>
+                        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200 text-black m-2" style="font-size: 1.9rem">"{{$quote->name}}"</p>
+                    </div>
+                @endforeach
+            @endif 
         </div>
     </div>
 </x-layout>
--- resources/views/quote.blade.php
+++ resources/views/quote.blade.php
@@ -1,15 +1,18 @@
 <x-layout>
-    <div class="flex min-h-screen items-center justify-center flex-col text-white">
-        <div class="block max-w-sm rounded-lg text-white dark:bg-neutral-700" >
+    <div class="flex flex-col w-[50%]">
+        <div class="grid gap-4 grid-cols-1">
             @if ($movie->quotes->count())
-                <img class="rounded-t-lg shadow-lg w-full"
-                    src="{{asset('storage/' . $movie->quotes->first()->thumbnail)}}"
-                    alt="" />
-                <div class="p-6">
-                <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200 text-3xl text-white">{{$movie->quotes->first()->name}}</p>
-                 @else
-                <a href="/movies/{{$movie->slug}}" class="underline underline-offset-1 text-center block text-3xl">{{$movie->title}}</a>
-            @endif
+                <div class="flex  items-center justify-center flex-col mt-10 mb-10">
+                    <div class="block max-w-sm rounded-lg text-white dark:bg-neutral-700 text-black w-full" >
+                        <img class="rounded-t-lg shadow-lg w-full"
+                            src="{{asset('storage/' . $movie->quotes->first()->thumbnail)}}"
+                            alt="" />
+                        <div class="p-6">
+                    </div>
+                    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200 text-4xl text-white text-center">"{{$movie->quotes->first()->name}}"</p>
+                </div>
+            @endif 
+        <a href="/movies/{{$movie->slug}}" class="underline underline-offset-1 text-center block text-3xl text-white">{{$movie->title}}</a>
         </div>
     </div>
 </x-layout>
