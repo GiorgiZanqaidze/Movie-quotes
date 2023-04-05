86aef18d8a200e257db15f4d1e3f094ce9b1cd9e
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-04-05 18:47:27 +0400

    style: tables

--- resources/views/movies-dashboard.blade.php
+++ resources/views/movies-dashboard.blade.php
@@ -1,14 +1,15 @@
 <x-layout>
-    <div class="flex flex-col ites-center w-[50%] gap-5"> 
-        @if ($movies)
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg w-full">
-            <h1 class="text-white">Movies</h1>
-                <table class="text-sm text-left text-gray-500 dark:text-gray-400 flex w-full">
-                    <tbody class="w-full">
+    @if ($movies)
+        <div class="relative overflow-x-auto sm:rounded-lg">
+            <h1 class="text-white text-center text-3xl mb-4">Movies</h1>
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 shadow-md ">
+                <tbody>
                     @foreach ($movies as $movie)
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 flex items-center">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                <p>{{$movie->title}}</p>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                                <div class="pl-3">
+                                    <div class="text-base font-semibold">{{$movie->title}}</div>
+                                </div>  
                             </th>
                             <td class="px-6 py-4">
                                 <form action="/movies/movie/{{$movie->slug}}/delete" method="POST">
@@ -22,9 +23,11 @@
                             </td>
                         </tr>
                     @endforeach
-                    </tbody>
-                </table>
-            </div>
-        @endif
-    </div>
+                </tbody>
+            </table>
+        </div>
+        @else
+        <h1 class="text-white">There are no movie</h1>
+    @endif
+
 </x-layout>
--- resources/views/quotes-dashboard.blade.php
+++ resources/views/quotes-dashboard.blade.php
@@ -1,15 +1,16 @@
 <x-layout>
-    <div class="flex flex-col ites-center w-[50%] gap-5"> 
-        @if ($quotes)
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <h1 class="text-white">Quotes</h1>
-                <table class="text-sm text-left text-gray-500 dark:text-gray-400 flex w-full">
-                    <tbody class='w-full'>
+    @if ($quotes)
+        <div class="relative overflow-x-auto sm:rounded-lg">
+            <h1 class="text-white text-center text-3xl mb-4">Quotes</h1>
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 shadow-md">
+                <tbody>
                     @foreach ($quotes as $quote)
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 flex items-center">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                <p class="break-words">{{$quote->name}}</p>
-                                <img src="{{asset('storage/' . $quote->thumbnail)}}" alt="" width="50px"/>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                                <img class="w-10 h-10 rounded-full" src="{{asset('storage/' . $quote->thumbnail)}}" alt="Jese image">
+                                <div class="pl-3">
+                                    <div class="text-base font-semibold">{{$quote->name}}</div>
+                                </div>  
                             </th>
                             <td class="px-6 py-4">
                                 <form action="/quotes/quote/{{$quote->slug}}/delete" method="POST">
@@ -23,9 +24,10 @@
                             </td>
                         </tr>
                     @endforeach
-                    </tbody>
-                </table>
-            </div>
-        @endif
-    </div>
+                </tbody>
+            </table>
+        </div>
+        @else
+        <h1 class="text-white">There are no quotes</h1>
+    @endif
 </x-layout>
