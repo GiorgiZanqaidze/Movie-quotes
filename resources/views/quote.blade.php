0f1ee35f5fdd8fab56b76055f1ffe0b3bea8a833
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-04-05 23:18:21 +0400

    wip: languages

--- resources/views/quote.blade.php
+++ resources/views/quote.blade.php
@@ -5,11 +5,11 @@
                 <div class="flex  items-center justify-center flex-col mt-10 mb-10">
                     <div class="block max-w-sm rounded-lg text-white dark:bg-neutral-700 text-black w-full" >
                         <img class="rounded-t-lg shadow-lg w-full"
-                            src="{{asset('storage/' . $movie->quotes->first()->thumbnail)}}"
+                            src="{{asset('storage/' . $movie->quotes->random()->thumbnail)}}"
                             alt="" />
                         <div class="p-6">
                     </div>
-                    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200 text-4xl text-white text-center">"{{$movie->quotes->first()->name}}"</p>
+                    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200 text-4xl text-white text-center">"{{$movie->quotes->random()->name}}"</p>
                 </div>
             @endif 
         <a href="/movies/{{$movie->slug}}" class="underline underline-offset-1 text-center block text-3xl text-white">{{$movie->title}}</a>
