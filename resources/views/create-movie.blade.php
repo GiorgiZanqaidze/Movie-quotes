7a2a3d5c3b2331ed110df2f3602b40044c5246ec
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-04-07 08:22:20 +0400

    feat: install spatie and update movie table

--- resources/views/create-movie.blade.php
+++ resources/views/create-movie.blade.php
@@ -9,20 +9,24 @@
                     </h1>
                     <form method="POST" action="/movies/movie/create" class="flex justify-center flex-col items-center" enctype="multipart/form-data">
                         @csrf
-                        <div class="mb-6">
-                            <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{__('forms.movie_title')}}</label>
-                            <input placeholder="{{__('placeholders.movie_name')}}" value="{{ old('title') }}" type="text" id="title" name="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            @error('title')
-                                <span class="text-xs text-red-500">{{$message}}</span>
-                            @enderror
-                        </div>
-                        <div class="mb-6">
-                            <label for="slug" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{__('forms.movie_slug')}}</label>
-                            <input placeholder='das-dsds-das-d-as-das-dasd-ad' value="{{ old('slug') }}" type="slug" id="slug" name="slug" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            @error('slug')
-                                <span class="text-xs text-red-500">{{$message}}</span>
-                            @enderror
-                        </div>
+                            <div>
+                            @foreach (config('app.available_locales') as $locale)
+                                <div class="mb-6">
+                                    <label for="title_{{$locale}}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{__('forms.movie_title')}} ({{ucwords($locale)}})</label>
+                                    <input placeholder="{{__('placeholders.movie_name')}}"  type="text" id="title_{{ $locale }}" name="title_{{ $locale }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    @error('title')
+                                        <span class="text-xs text-red-500">{{$message}}</span>
+                                    @enderror
+                                </div>
+                            @endforeach
+                                <div class="mb-6">
+                                    <label for="slug" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{__('forms.movie_slug')}}</label>
+                                    <input placeholder='das-dsds-das-d-as-das-dasd-ad' value="{{ old('slug') }}" type="slug" id="slug" name="slug" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    @error('slug')
+                                        <span class="text-xs text-red-500">{{$message}}</span>
+                                    @enderror
+                                </div>
+                            </div>
                         <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{{__('forms.add')}}</button>
                     </form>
                 </div>
