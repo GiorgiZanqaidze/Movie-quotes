0a34279f7e53dc884aca60ddd0ad7677ad75b740
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-04-12 18:42:55 +0400

    refactor: delete unneccesary code

--- app/Http/Requests/FormRequest/StoreMovieRequest.php
+++ app/Http/Requests/FormRequest/StoreMovieRequest.php
@@ -6,11 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreMovieRequest extends FormRequest
 {
-	public function authorize()
-	{
-		return true;
-	}
-
 	public function rules()
 	{
 		$rules = [
