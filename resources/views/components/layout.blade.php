dc088bc4301ac7c67bf4dbb757bcf85a5c7c7801
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-04-13 15:39:27 +0400

    chore: remove  comments

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -19,14 +19,14 @@
 <body class="bg-gray-500 relative">
     <div class="flex justify-center">
         <aside class="flex flex-col justify-center items-center gap-2 m-4 fixed left-0" style="top: 50%; transform: translateY(-50%)">
-            {{-- @foreach (Config::get('languages') as $lang => $language)
+            @foreach (Config::get('languages') as $lang => $language)
                 @if ($lang != App::getLocale())
                         <a class="hover:bg-white hover:text-black text-white font-bold py-2 px-4 border-white border-2 bg-transparent rounded rounded-full" href="{{ route('lang.switch', $lang) }}">{{$language}}</a>
                 @endif
                 @if ($lang === App::getLocale())
                         <a class="hover:bg-white hover:text-black text-black font-bold py-2 px-4 border-white border-2 bg-transparent rounded rounded-full bg-white" href="{{ route('lang.switch', $lang) }}"> {{$language}}</a>
                 @endif
-            @endforeach --}}
+            @endforeach
             @auth
                 <div>
                     <form action="/logout" method="POST">
